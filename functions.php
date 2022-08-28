415a9194cfeaa7d94bd2319485ad938694e1d313
Ronbout <ronbout@yahoo.com>
2022-08-27 23:02:36 -0500

    Added a shortcode for the near future that can be added to the site header for placement of an ad, which is dependent on the page slug.  The code is commented out for now.

--- functions.php
+++ functions.php
@@ -30,3 +30,39 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
  * Include the functions related to the jobs board
  */
 include MANNA_PRESS_CHILD_THEME_DIR . 'jobs_functions.php';
+
+/*
+function taste_ads_add_shortcodes() {
+	add_shortcode("AD-DISPLAY", "display_header_ad");
+}
+add_action("init", "taste_ads_add_shortcodes");
+
+function display_header_ad() {
+	global $wp;
+	$page_slug = add_query_arg( array(), $wp->request );
+	switch ($page_slug) {
+		case "jobs-listing":
+			?>
+				<div style="width: 628px; height: 100px" >
+					<a href="https://dingledistillery.ie/" target="_blank">
+						<img width="628" height="100" style="height: 100px"
+						 src="https://www.thetaste.ie/wp-content/uploads/2021/09/dinlge-banner-1400.jpg" alt="">
+					</a>
+				</div>
+			<?php
+			break;
+		default:
+			?>
+			<div>
+				<div id='div-gpt-ad-1650539070276-0' style='min-width: 728px; min-height: 90px;'>
+					<script>
+						googletag.cmd.push(function() { googletag.display('div-gpt-ad-1650539070276-0'); });
+					</script>
+				</div>
+			</div>
+			<?php
+	}
+	?>
+	<?php	
+}
+*/
