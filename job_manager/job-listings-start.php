758f01bb14d3e3a19758eb76c5e5cd8f56c125e1
Ronbout <ronbout@yahoo.com>
2022-08-29 07:47:01 -0500

    Add the ucdsu logo to the jobs listing page in the job listing start template.  Also pulled down package-selection template from wc paid listings for upcoming task, but with no changes yet.

---
+++ job_manager/job-listings-start.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Content shown before job listings in `[jobs]` shortcode.
+ *
+ * This template can be overridden by copying it to yourtheme/job_manager/job-listings-start.php.
+ *
+ * @see         https://wpjobmanager.com/document/template-overrides/
+ * @author      Automattic
+ * @package     wp-job-manager
+ * @category    Template
+ * @version     1.15.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<div id="job-listing-ucdsu-logo">
+	<span>Supported By </span>
+	<img src="https://www.thetaste.ie/wp-content/uploads/2022/08/UCDSU_Logo.png" title="" alt="">
+</div>
+<ul class="job_listings">
